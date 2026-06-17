7ba10c351d9f99c185a9bf2493300f9467378f3b
7ames <123058@combertonvc.org>
2026-06-17 14:28:26 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-echo -n "Enter API key: " && read -s KEY_INPUT && echo "" && echo "" >> ~/.bashrc && echo "export GROQ_API_KEY=\"\$KEY_INPUT\"" >> ~/.bashrc && source ~/.bashrc
+mkdir -p key && echo -n "Enter API key: " && read -s KEY_INPUT && echo "" && echo -n "$KEY_INPUT" > key/raw.txt
 pip install -r requirements.txt
 echo "alias nova=\"python $(pwd)/src/transpiler.py\"" >> ~/.bashrc && source ~/.bashrc
